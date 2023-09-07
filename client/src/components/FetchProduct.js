fb16c5e8839fd5bb4ef287b43b3a9446b570c129
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2023-09-07 18:43:13 +0530

    fetch data from backedn and display

--- client/src/components/FetchProduct.js
+++ client/src/components/FetchProduct.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const FetchProduct = () => {
+function ProductDetails() {
+  const [url, setUrl] = useState('');
+  const [productDetails, setProductDetails] = useState(null);
+
+  const fetchProductDetails = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/product_details', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ url }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Request failed');
+      }
+
+      const data = await response.json();
+      setProductDetails(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>FetchProduct</div>
-  )
+    <div>
+      <input
+        type="text"
+        placeholder="Enter Amazon URL"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <button onClick={fetchProductDetails}>Fetch Details</button>
+      {productDetails && (
+        <div>
+          <p>{productDetails.title}</p>
+          <p>{productDetails.description}</p>
+          <img src={productDetails.photos} alt="Product" />
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default FetchProduct
+export default ProductDetails;
--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const openai = require('openai');
 const puppeteer = require('puppeteer');
 
-
 const app = express();
 const port = 5000;
 
@@ -87,39 +86,67 @@ app.post('/sustain', async (req, res) => {
 });
 
 
-async function start(url){
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+//get product details
+app.post('/product_details', async (req, res) => {
+  try {
+    const { url } = req.body;
+    const productDetails = await start(url);
+    res.json(productDetails);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+async function start(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  const photos = await page.$$eval('#landingImage', (imgs) => {
+    return imgs.map((img) => img.src);
+  });
+  const title = await page.$eval('#productTitle', (title) => {
+    return title.innerText;
+  });
+  const description = await page.$eval('#productDescription', (description) => {
+    return description.innerText;
+  });
+  const productDetails = {
+    photos: photos[0],
+    title: title,
+    description: description,
+  };
+  await browser.close();
 
 
-    const photos = await page.$$eval("#landingImage" , (imgs) => {
-        return imgs.map((img) => img.src);
-    })
-    const title = await page.$eval("#productTitle" , (title) => {
-        return title.innerText;
-    })
-    const description = await page.$eval("#productDescription" , (description) => {
-        return description.innerText;
-    })
+  const sustainAttributes = [
+    'organic',
+    'fair trade',
+    'recyclable',
+    'reusable',
+    'renewable energy',
+    'energy efficient',
+    'sustainable packaging',
+    'biodegradable/compostable',
+    'low carbon footprint',
+    'carbon neutral',
+    'ethically sourced',
+    'water conservation',
+    'non-toxic',
+    'cruelty-free',
+    'upcycled',
+    'locally produced',
+    'social impact',
+    'low/zero VOC'
+];
+const sustainableAttributesToString = sustainAttributes.join(',');
+const prompt = `I have a product titled '${title}' and product description '${description}'. Match the product with sustainable attributes and explain why. Sustainable attributes: ${sustainableAttributesToString}. If the product can't match any of the sustainable attributes, return False.\nReturn a dictionary like this: {'attribute': 'Explanation'}`;
+console.log(prompt);
 
-    const product_details = {
-        "photos" : photos[0],
-        "title" : title,
-        "description" : description
-    }
 
-    console.log(product_details);
-
-
-    await browser.close();
+  return productDetails;
 }
 
-start('https://www.amazon.in/iQOO-Stellar-Snapdragon-Segment-Included/dp/B07WHSR1NR/?_encoding=UTF8&pd_rd_w=ki2jG&content-id=amzn1.sym.b5a625fa-e3eb-4301-a9e2-f9c8b3e7badf%3Aamzn1.symc.36bd837a-d66d-47d1-8457-ffe9a9f3ddab&pf_rd_p=b5a625fa-e3eb-4301-a9e2-f9c8b3e7badf&pf_rd_r=79ZXSFG5XYJXZ073S65P&pd_rd_wg=HphIF&pd_rd_r=46f9ff55-90c2-4629-9cb4-300ec1000410&ref_=pd_gw_ci_mcx_mr_hp_atf_m&th=1')
-
-
-
-//test
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
